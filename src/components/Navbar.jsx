dfc79ca383565e1b918e99121e2a64789b21ba22
Pruthvi shetty <pruthvishetty1721@gmail.com>
2025-07-19 14:05:42 +0530

    Added logo to Header and updated footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,16 @@ export default function Header() {
   return (
     <header className="bg-blue-700 text-white p-4">
       <nav className="flex justify-between items-center">
-        <h1 className="text-xl font-bold">Vishwa Bharati</h1>
+        <div className="flex items-center gap-2">
+          {/* Logo */}
+          <img
+            src="https://tse4.mm.bing.net/th/id/OIP.dYYZuUl1R4RwUTtFMrCitgAAAA?pid=Api&P=0&h=180"
+            alt="Logo"
+            className="w-10 h-8 object-contain"
+          />
+          <h1 className="text-xl font-bold">Vishwa Bharati</h1>
+        </div>
+        
         <ul className="flex gap-4">
           <li><Link to="/" className="hover:underline">Home</Link></li>
           <li><Link to="/about" className="hover:underline">About</Link></li>
