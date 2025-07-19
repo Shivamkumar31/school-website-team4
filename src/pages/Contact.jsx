07c09015d15145ece9c4754ad38f7817f18462f7
Satyam Singh <satyamsinghksh@gmail.com>
2025-07-19 14:32:51 +0530

    Add headings to About, Contact, Registration, and Student pages

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const contact = () => {
   return (
     <div>
-      
+      <h1>this is contact page</h1>
     </div>
   )
 }
--- src/pages/Student.jsx
+++ src/pages/Student.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Student = () => {
   return (
     <div>
-      
+      <h4>this is student page</h4>
     </div>
   )
 }
